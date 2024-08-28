a658fbfb91a627cb38b6597ae93a5e57370b80d3
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2024-08-28 14:41:45 +0700

    init firebase conn

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,14 @@
 var Content = () => {
-    return (
-        <main className="bg-grey-50 mb-auto h-10 flex-grow">
-            <div className="grid grid-cols-3 my-4">
-                <div className=""></div>
+return (
+    <main className="bg-grey-50 mb-auto h-10 flex-grow">
+        <div className="grid grid-cols-6 my-4">
+            <div className="col-start-2 col-span-4">
                 <div className="bg-white rounded-md h-96 shadow-md p-5">
-                    <h1>Hello</h1>
+                    <h1 className="text-4xl">Selamat Pagi</h1>
                 </div>
-                <div className=""></div>
             </div>
-        </main>
+        </div>
+    </main>
     )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Foot = () => {
     return (
         <footer className="bg-white flex p-3 items-center mb-auto justify-center bottom-0">
-            <p>&copy; 2024 Google</p>
+            <p>&copy; 2024 Bukan Google</p>
         </footer>
     )
 }
